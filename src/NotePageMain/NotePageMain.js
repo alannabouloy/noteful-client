f8eb8f2b354b737e16f59a25b8785c1049a188e3
Alanna Bouloy <lanihope@gmail.com>
2020-11-30 17:13:38 -0500

    fixed delete glitch

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -28,6 +28,7 @@ export default class NotePageMain extends React.Component {
           name={note.note_name}
           modified={note.modified}
           onDeleteNote={this.handleDeleteNote}
+          history={this.props.history}
         />
         <div className='NotePageMain__content'>
           {note.content.split(/\n \r|\n/).map((para, i) =>
