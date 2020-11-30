b41447da8dd21221f937a5367250a12591488147
Alanna Bouloy <lanihope@gmail.com>
2020-11-30 15:39:24 -0500

    adjusted code

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000',
-  TOKEN_KEY: 'fd4d8b2e-333a-11eb-adc1-0242ac120002'
+  API_ENDPOINT: 'https://safe-springs-23611.herokuapp.com',
+  TOKEN_KEY: 'f8b3cf2e-3349-11eb-adc1-0242ac120002'
 }
